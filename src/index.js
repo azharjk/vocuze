dc9e45304c0f062df61f3718bf8091157211de07
azharjk <azhr.project@gmail.com>
2021-07-16 16:34:25 +0700

    Added entrypoint to start the application
     - Initialize Hapi server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import Hapi from '@hapi/hapi';
+
+const PORT = 5000
+
+const init = async () => {
+  const server = Hapi.server({
+    port: PORT,
+    host: 'localhost'
+  });
+
+  await server.start();
+  console.log(`Application running on port ${PORT}`);
+}
+
+init();
