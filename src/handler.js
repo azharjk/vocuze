46e8448a710600a12f4b29e14c0574c31e047bb6
azharjk <azhr.project@gmail.com>
2021-07-16 17:09:13 +0700

    Added `POST` method handler to `/books`

--- src/handler.js
+++ src/handler.js
@@ -1,3 +1,77 @@
+import { nanoid } from 'nanoid';
+
+const books = [];
+
 export const CreateBookHandler = (request, h) => {
-  return 'Should create a new book';
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading
+  } = request.payload
+
+  if (name === undefined) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku'
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
+    });
+    response.code(400);
+    return response;
+  }
+
+  const id = nanoid(16);
+  const finished = pageCount === readPage;
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+
+  books.push({
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt
+  });
+
+  const OK = books.filter(book => book.id === id);
+
+  // Succesfully created book
+  if (OK) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id
+      }
+    });
+    response.code(201);
+    return response;
+  }
+
+  // Generic error
+  const response = h.response({
+    status: 'error',
+    message: 'Buku gagal ditambahkan'
+  });
+  response.code(500);
+  return response;
 }
