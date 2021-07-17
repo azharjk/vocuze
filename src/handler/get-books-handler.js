49e77aadcbbda6924e37330af21af71068c4f7c0
azharjk <azhr.project@gmail.com>
2021-07-17 10:49:24 +0700

    Added query filter
     - Added for name, reading, and finished

--- src/handler/get-books-handler.js
+++ src/handler/get-books-handler.js
@@ -1,10 +1,34 @@
 import books from './books.js';
 
 export default (request, h) => {
+  const {
+    name,
+    reading,
+    finished,
+  } = request.query;
+
+  let filteredBooks = books;
+
+  if (name) {
+    filteredBooks = books.filter((b) => b.name.toLowerCase().includes(name.toLowerCase()));
+  }
+
+  if (reading === '1') {
+    filteredBooks = books.filter((b) => b.reading);
+  } else if (reading === '0') {
+    filteredBooks = books.filter((b) => !b.reading);
+  }
+
+  if (finished === '1') {
+    filteredBooks = books.filter((b) => b.finished);
+  } else if (finished === '0') {
+    filteredBooks = books.filter((b) => !b.finished);
+  }
+
   const response = h.response({
     status: 'success',
     data: {
-      books: books.map((b) => ({ id: b.id, name: b.name, publisher: b.publisher })),
+      books: filteredBooks.map((b) => ({ id: b.id, name: b.name, publisher: b.publisher })),
     },
   });
   response.code(200);
