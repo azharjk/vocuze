7c098315614582cdc95b8cc1c2db3495eb55f48e
azharjk <azhr.project@gmail.com>
2021-07-17 09:55:48 +0700

    Added `PUT` to update the book

--- src/route.js
+++ src/route.js
@@ -1,6 +1,7 @@
 import CreateBookHandler from './handler/create-book-handler.js';
 import GetBookHandler from './handler/get-book-handler.js';
 import GetBooksHandler from './handler/get-books-handler.js';
+import UpdateBookHandler from './handler/update-book-handler.js';
 
 export default [
   {
@@ -17,5 +18,10 @@ export default [
     method: 'GET',
     path: '/books/{bookId}',
     handler: GetBookHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: UpdateBookHandler,
   },
 ];
